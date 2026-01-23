210b8ef244b1f6ef967c273884b090ecec5655a0
Prajwal M V <mvprajwal546@gmail.com>
2026-01-23 11:54:34 +0530

    Required Propagation which is Default Propagation.

--- src/main/java/com/codesnippet/BookApplication/Service/BookService.java
+++ src/main/java/com/codesnippet/BookApplication/Service/BookService.java
@@ -3,6 +3,7 @@ package com.codesnippet.BookApplication.Service;
 import com.codesnippet.BookApplication.Entity.Book;
 import com.codesnippet.BookApplication.Repository.BookRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -15,8 +16,11 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     //Transaction is applicable only for public methods
+    //If the method which is calling this method if it has  transactional annotation then
+    //this method will join same transaction or else it will create a new .
+    // Default Transaction Propagation is Required which I explained above
     public Book addBook(Book book) {
         bookRepository.save(book);
         return book;
